af83a12e4c65e987a70da4c8c96e9bb1629503cb
Lexq1 <76948335+Lexq1@users.noreply.github.com>
2022-07-10 23:37:20 +0300

    Add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+  let strNumb = '' + numb;
+  let strNumbRev = '';
+  for(let i = strNumb.length - 1; i >= 0; i--){
+  	strNumbRev += strNumb[i];
+  }
   
+  return +strNumbRev;
 }
