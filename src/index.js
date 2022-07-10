b03d9f050dea9f8c4d1214d273348d61f478c1fd
Lexq1 <76948335+Lexq1@users.noreply.github.com>
2022-07-11 00:14:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+  let strNumb = '' + n;
+  let strNumbRev = '';
+  for(let i = strNumb.length - 1; i >= 0; i--){
+    if(strNumb[i] == '-'){
+        strNumbRev += '';
+    }else{
+        strNumbRev += strNumb[i];
+    }
+  }
   
+  return +strNumbRev;
 }
